dd6f5c49c03fa0f561e30163bad3d05d43924c22
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-10-02 17:03:55 +0200

    add series

--- data/db.php
+++ data/db.php
@@ -3,35 +3,65 @@ $productions_data = [
   [
     "title" => "Indiana Jones e i predatori dell'arca perduta",
     "genres" => ["Azione", "Avventura"],
-    "year" => 1981,
+    "published_year" => 1981,
     "poster" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTphCa50-9QAmsVxZ13pUyKbCIHoS610tu3Hf7Dr1a-J4pfKkKJ"
   ],
   [
     "title" => "Indiana Jones e il tempio maledetto",
     "genres" => ["Azione", "Avventura"],
-    "year" => 1984,
+    "published_year" => 1984,
     "poster" => "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTcj0A_ZioPLi3dVT1cu-Ppl6TBpXJQVjdX_OW51NZB6rmcrzjw"
 
   ],
   [
     "title" => "Indiana Jones e l'ultima crociata",
     "genres" => ["Azione", "Avventura"],
-    "year" => 1989,
+    "published_year" => 1989,
     "poster" => "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQLKBIq95zuQzPBanSAAubpREoFmpX08Rnq6L2Lxm9UFUhTC74c"
 
   ],
   [
     "title" => "Indiana Jones e il regno del teschio di cristallo",
     "genres" => ["Azione", "Avventura"],
-    "year" => 2008,
+    "published_year" => 2008,
     "poster" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-eVLJYYjLSU_k0SXoaEX0sVteM2YN23VLmA0RXvAxm38aBqCA"
 
   ],
   [
     "title" => "Indiana Jones 5",
     "genres" => ["Azione", "Avventura"],
-    "year" => 2023,
+    "published_year" => 2023,
     "poster" => "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRx7OdWeoL_KV9ACpHksuD4I67KzgJTGV9LJLt2vnFK5drBJPG0"
+
+  ],
+  [
+    "title" => "Ahsoka ",
+    "genres" => ["Fantascienza", "Azione", "Avventura", "Drammatico"],
+    "aired_from_year" => 2023,
+    "aired_to_year" => 2023,
+    "number_of_episodes" => 8,
+    "number_of_seasons" => 1,
+    "poster" => "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT0EJBVCDxFu1ExCWJWOgCjhcF54Jxf20N_dtTVHuoorDRn2xyi"
+
+  ],
+  [
+    "title" => "The Bear",
+    "genres" => ["Commedia", "Drammatica"],
+    "aired_from_year" => 2022,
+    "aired_to_year" => "in produzione",
+    "number_of_episodes" => 18,
+    "number_of_seasons" => 2,
+    "poster" => "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/9531676CAD1BDA30FD35DFE0A280FCBFFD48D62CB79428389EC127336B9F2DB1/scale?width=1200&aspectRatio=1.78&format=jpeg"
+
+  ],
+  [
+    "title" => "Peaky Blinders",
+    "genres" => ["storico", "gangster", "drammatico", "in costume", "noir"],
+    "aired_from_year" => 2013,
+    "aired_to_year" => 2022,
+    "number_of_episodes" => 36,
+    "number_of_seasons" => 6,
+    "poster" => "hhttps://m.media-amazon.com/images/M/MV5BZjYzZDgzMmYtYjY5Zi00YTk1LThhMDYtNjFlNzM4MTZhYzgyXkEyXkFqcGdeQXVyMTE5NDQ1MzQ3._V1_.jpg"
 
   ],
 
