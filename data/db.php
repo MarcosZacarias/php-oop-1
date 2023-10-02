119b87eb21542f17dbe66b2f8c2bef21411c0304
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-10-02 17:18:36 +0200

    add running time movie

--- data/db.php
+++ data/db.php
@@ -4,12 +4,14 @@ $movies_data = [
     "title" => "Indiana Jones e i predatori dell'arca perduta",
     "genres" => ["Azione", "Avventura"],
     "published_year" => 1981,
+    "running_time" => 115,
     "poster" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTphCa50-9QAmsVxZ13pUyKbCIHoS610tu3Hf7Dr1a-J4pfKkKJ"
   ],
   [
     "title" => "Indiana Jones e il tempio maledetto",
     "genres" => ["Azione", "Avventura"],
     "published_year" => 1984,
+    "running_time" => 118,
     "poster" => "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTcj0A_ZioPLi3dVT1cu-Ppl6TBpXJQVjdX_OW51NZB6rmcrzjw"
 
   ],
@@ -17,6 +19,7 @@ $movies_data = [
     "title" => "Indiana Jones e l'ultima crociata",
     "genres" => ["Azione", "Avventura"],
     "published_year" => 1989,
+    "running_time" => 127,
     "poster" => "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQLKBIq95zuQzPBanSAAubpREoFmpX08Rnq6L2Lxm9UFUhTC74c"
 
   ],
@@ -24,6 +27,7 @@ $movies_data = [
     "title" => "Indiana Jones e il regno del teschio di cristallo",
     "genres" => ["Azione", "Avventura"],
     "published_year" => 2008,
+    "running_time" => 122,
     "poster" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-eVLJYYjLSU_k0SXoaEX0sVteM2YN23VLmA0RXvAxm38aBqCA"
 
   ],
@@ -31,6 +35,7 @@ $movies_data = [
     "title" => "Indiana Jones 5",
     "genres" => ["Azione", "Avventura"],
     "published_year" => 2023,
+    "running_time" => 154,
     "poster" => "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRx7OdWeoL_KV9ACpHksuD4I67KzgJTGV9LJLt2vnFK5drBJPG0"
 
   ],
