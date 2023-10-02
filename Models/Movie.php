22d9419f37d2242c26e632964a0fa441b53de7e9
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-10-02 17:55:06 +0200

    add ovveride function get Details for Movie

--- Models/Movie.php
+++ Models/Movie.php
@@ -2,6 +2,7 @@
 
 class Movie extends Production
 {
+  public static $time_unit_measure = "min";
   public $published_year;
   public $running_time;
   public function __construct(
@@ -14,5 +15,31 @@ class Movie extends Production
     parent::__construct($_title, $_genres, $_poster);
     $this->published_year = $_published_year;
     $this->running_time = $_running_time;
+  }
+
+  public function getDetails()
+  {
+    $um = self::$time_unit_measure;
+    return "
+    <p>
+      <strong>Titolo: </strong>
+      $this->title
+    </p>
+    <p>
+      <!-- Loop foreach genre in list Genres -->
+      <span>
+        <strong>Generi: </strong>
+        {$this->get_all_genres()}
+      </span>
+    </p>
+    <p>
+      <strong>Anno di uscita: </strong>
+      $this->published_year
+    </p>
+    <p>
+      <strong>Durata: </strong>
+      $this->running_time $um
+    </p>";
+
   }
 }
