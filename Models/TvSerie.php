368183cc5ff34d1f58a9849386e44c38dd613b19
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-10-02 18:00:35 +0200

    add override function get detaills for TvSeries

--- Models/TvSerie.php
+++ Models/TvSerie.php
@@ -23,4 +23,36 @@ class TvSerie extends Production
     $this->number_of_seasons = $_number_of_seasons;
 
   }
+  public function getDetails()
+  {
+    return "
+    <p>
+      <strong>Titolo: </strong>
+      $this->title
+    </p>
+    <p>
+      <!-- Loop foreach genre in list Genres -->
+      <span>
+        <strong>Generi: </strong>
+        {$this->get_all_genres()}
+      </span>
+    </p>
+    <p>
+      <strong>Emessa dall'anno: </strong>
+      $this->aired_from_year
+    </p>
+    <p>
+      <strong>Fino all'anno: </strong>
+      $this->aired_to_year
+    </p>
+    <p>
+      <strong>Numeri di episodi: </strong>
+      $this->number_of_episodes
+    </p>
+    <p>
+      <strong>Numeri di stagioni: </strong>
+      $this->number_of_seasons
+    </p>";
+
+  }
 }
